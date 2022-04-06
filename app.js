bc5a63ef08a4bd290e622098d74f3b6ab335ea6a
parisote <detomas@gmail.com>
2022-04-06 00:50:30 -0300

    feat: avanzo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TELEGRAM_BOT;
@@ -17,21 +18,57 @@ bot.on('message', (msg) => {
 bot.onText(/\/new_alert/, async (msg) => {  
     const chatId = msg.chat.id;
 
-    const opts = {
+    /*const opts = {
       reply_to_message_id: msg.message_id,
       reply_markup: {
         "keyboard": [
-          [{ text: "Ethereum" }],
-          [{ text: "Bitcoin" }],
-          [{ text: "Matic" }]
-        ]        
+          [{ text: "Ethereum"}],
+          [{ text: "Bitcoin"}],
+          [{ text: "Matic"}]
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true,
+        force_reply: true,
+      }
+    };*/
+
+    const opts = {
+      reply_markup: {
+        inline_keyboard: [[
+            {
+                text: 'Ethereum',
+                callback_data: 'Ethereum'
+            }, {
+                text: 'Bitcoin',
+                callback_data: 'Bitcoin'
+            }, {
+                text: 'Matic',
+                callback_data: 'Matic'
+            }
+        ]]
       }
     };
 
-    bot.sendMessage(chatId, 'Sobre que coin quiere crear la alerta?', opts)
-    .then(() => {
-      answerCallbacks[chatId] = function(msg) {
-        bot.sendMessage(msg.chat.id, "Cual es el precio sobre el cual desea alertar sobre "+ msg.text + "?")
-      };
-    });
+    bot.sendMessage(chatId, 'Sobre que coin quiere crear la alerta?', opts);
+});
+
+// Listener (handler) for callback data from /label command
+bot.on('callback_query', (callbackQuery) => {
+  const message = callbackQuery.message;
+  const category = callbackQuery.data;
+
+  bot.sendMessage(message.chat.id, `Cual es el precio sobre el cual desea alertar sobre "${category}"?`)
+  .then(() => answerCallbacks[message.chat.id] = async (msg) => {
+    axios.post('http://localhost:3000/api',{
+      coin: category,
+      price: msg.text,
+      who: msg.chat.id
+    })
+    .then(res => { 
+      bot.sendMessage(message.chat.id, "Alerta guardada correctamente");
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  })
 });
